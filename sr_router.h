341885cbb9e2b96fdcb8452846fc85ca0264902c
Fate9834 <alexece.wu@mail.utoronto.ca>
2015-12-09 19:46:55 -0500

    fix

--- sr_router.h
+++ sr_router.h
@@ -100,6 +100,6 @@ void sr_print_if_list(struct sr_instance* );
 /* -- sr_nat.c -- */
 
 void nat_handle_ippacket(struct sr_instance *sr, sr_ip_hdr_t *ipPacket,
-                              unsigned int length, struct sr_if const *const r_interface);
+                              unsigned int length, struct sr_if *r_interface);
 
 #endif /* SR_ROUTER_H */
