7fc1d074ef7b8291adb2ca75f66085c342307e1a
Zhao Wen <you@example.com>
2015-12-10 16:36:56 -0500

    ~

---
+++ bin/sr_arpcache.d
@@ -0,0 +1,14 @@
+bin/sr_arpcache.o: sr_arpcache.c sr_arpcache.h sr_if.h sr_protocol.h \
+ sr_router.h sr_nat.h sr_rt.h
+
+sr_arpcache.h:
+
+sr_if.h:
+
+sr_protocol.h:
+
+sr_router.h:
+
+sr_nat.h:
+
+sr_rt.h:
---
+++ bin/sr_router.d
@@ -0,0 +1,16 @@
+bin/sr_router.o: sr_router.c sr_if.h sr_protocol.h sr_rt.h sr_router.h \
+ sr_arpcache.h sr_nat.h sr_utils.h
+
+sr_if.h:
+
+sr_protocol.h:
+
+sr_rt.h:
+
+sr_router.h:
+
+sr_arpcache.h:
+
+sr_nat.h:
+
+sr_utils.h:
